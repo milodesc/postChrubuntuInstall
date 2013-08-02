63a9068af75c4d99d8b306e581d29d355a74cf90
patrick <patrick@ChrUbuntu.(none)>
2013-08-01 20:45:46 -0400

    Commenting out live commands so they're not run accidentally.

--- postChrubuntuInstallation.sh
+++ postChrubuntuInstallation.sh
@@ -11,7 +11,7 @@
 #Create script for rebooting into ChromeOS
 ####echo '#!/bin/sh' > ~/Desktop/rebootToChromeOS.sh
 ####echo 'sudo cgpt add -i 6 -P 0 -S 1 /dev/sda' >> ~/Desktop/rebootToChromeOS.sh
-####echo 'sudo reboot' >> ~/Desktop/rebootToChromeOS.sh
+####echo 'reboot' >> ~/Desktop/rebootToChromeOS.sh
 ####chmod 755 ~/Desktop/rebootToChromeOS.sh
 
 #############################
@@ -19,8 +19,8 @@
 #############################
 
 #Fix CPU spike after closing lid to suspend. see http://www.cidgendered.com/?p=9
-stop acpid
-echo 'manual' > /etc/init/acpid.override
+#stop acpid
+#echo 'manual' > /etc/init/acpid.override
 
 #Install xdotool for enabling shortcut buttons
 #apt-get install xdotool
